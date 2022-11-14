e589c37d7bea88660d7b870e78941099953958be
ResistanceTo <48614752+ResistanceTo@users.noreply.github.com>
2022-11-14 20:29:49 +0800

    fix error

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import tornado.web
 from urls import urls
-from settings import logging, LISTEN_PORT, LISTEN_HOST, SCHED_MINUTE, SCHED_HOUR
+from settings import logging, LISTEN_PORT, LISTEN_HOST, SCHED_MINUTE, SCHED_HOUR, WHITELIST
 from src.utils.wechat import weather_today
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -12,7 +12,7 @@ async def main():
     app = tornado.web.Application(urls, default_host=LISTEN_HOST)
     app.listen(LISTEN_PORT)
     logging.info("项目已启动: http://{}:{}/".format(LISTEN_HOST, LISTEN_PORT))
-    sched.add_job(weather_today, "cron", minute=SCHED_MINUTE, hour=SCHED_HOUR)
+    sched.add_job(weather_today, "cron", args=[WHITELIST], minute=SCHED_MINUTE, hour=SCHED_HOUR)
     sched.start()
     await asyncio.Event().wait()
 
